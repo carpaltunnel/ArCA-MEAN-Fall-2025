3583553bf76151d1880de272e15cfbc3fb9db910
CC <a_fake_address@uca.edu>
2025-08-27 21:00:27 -0500

    musicbrainz

--- 028-REST APIs (Using)/code/axios-example/musicbrainz.js
+++ 028-REST APIs (Using)/code/axios-example/musicbrainz.js
@@ -1,37 +1,15 @@
-const axios = require('axios');
-/*
+import axios from 'axios';
 
-https://musicbrainz.org/doc/MusicBrainz_API/Examples
+const searchTerm = process.argv[2];
 
-https://musicbrainz.org/ws/2/artist?inc=releases&fmt=json&query=jinjer
+const url = `https://musicbrainz.org/ws/2/artist?query=${searchTerm}`
+const searchResults = await axios.get(url);
+const myArtistId = searchResults.data.artists[0].id;
 
-https://musicbrainz.org/ws/2/artist/51b37017-859c-465e-8810-2d2dd41a401e?inc=releases&fmt=json
+const releasesUrl = `https://musicbrainz.org/ws/2/artist/${myArtistId}?inc=releases`;
+const releaseResponse = await axios.get(releasesUrl);
 
-
-
-*/
-
-const artist = 'Jinjer';
-
-const getUrl = async (url) => {
-  const result = await axios.get(url);
-  //console.log(`Status Code : ${result.status}\nData :\n ${JSON.stringify(result.data, 2, 2)}`);
-  return result.data;
+console.log(`All releases by the band ${searchTerm} (${myArtistId})`);
+for (let release of releaseResponse.data.releases) {
+  console.log(`\t[${release.date}] ${release.title}`);
 }
-
-const main = async () => {
-  const artistResults = await getUrl(`https://musicbrainz.org/ws/2/artist?inc=releases&fmt=json&query=${artist}`);
-
-  console.log(`The ID is : ${artistResults.artists[0].id}`);
-  const artistId = artistResults.artists[0].id;
-
-  const releaseResults = await getUrl(`https://musicbrainz.org/ws/2/artist/${artistId}?inc=releases&fmt=json`);
-  console.log(`There are ${releaseResults.releases.length} releases for the artist "${artist}" and they are :`);
-
-  let counter = 1;
-  releaseResults.releases.forEach((release) => {
-    console.log(`${counter++}. ${release.title} (${release.date})`);
-  });
-};
-
-main();
